a4ed75ebfa75427a38a4dacd0acfe2be346f084e
Richd0tcom <86890896+Richd0tcom@users.noreply.github.com>
2022-07-01 12:33:18 +0100

    Feat: Add About page
    Added JSX and styling for About page.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,66 +1,32 @@
 import React from "react";
-import HomeNav from '../Navbars/HomeNav';
-import smtext from './assets/About Us (1).png';
-import lgtext from './assets/About Us.png';
-import what from './assets/Frame 12.png';
-import whatlg from './assets/Frame 12 (1).png';
-import why from './assets/Frame 13.png';
-import whylg from './assets/Frame 13 (1).png';
-import arrow from './assets/Vector (1).png';
-import bar from './assets/Frame 14.png';
 
 function About() {
   return(
     <React.Fragment>
-        <div className="bg-white h-auto md:pb-16">
-          <HomeNav />
-          <div className="block md:hidden">
-              <img src={smtext} alt="About Us" />
-          </div>
-          <div className="pt-9 hidden md:flex">
-            <img src={lgtext} alt="About Us" />
-          </div>
-          {/* card */}
-          <div className='flex flex-col justify-center items-center pt-5 md:hidden'>
-            {/* 1 */}
-            <div className='pb-8'>
-              <img src={what} alt="" />
-            </div>
-            {/* 2 */}
-            <div>
-              <img src={why} alt="" />
-            </div>
-          </div>
-          <div className='bg-[#2B5EC0] w-[414px] h-auto mt-4 mb-[21px] text-white md:hidden'>
-            <h1 className='font-[Open Sans] font-[700] text-[20px] tracking-wide pt-[20.38px] pl-[40px]'>Benefits of using eDucura?</h1>
-            <p className="tracking-wider pl-[40px] pb-3">We have a world class simple and <br />easy-to-use software that is customized uniquely for your school. <span className="font-[700] tracking-wider">It takes less<br /> than 5 mintes to set up an account<br /> with us.</span></p>
-              <div className="flex justify-end pr-5 pb-5">
-                <div className="flex items-center">
-                  <div className="mr-2">
-                    <h1 className="font-[11px] font-[400]">Click here to begin</h1>
-                  </div>
-                  <div>
-                    <img src={arrow} alt="arrow" />
-                  </div>
-                </div>
-              </div>
-          </div>
-          {/* lg */}
-          <div className="md:flex gap-16 justify-center pt-9 hidden">
-            {/* 1 */}
-            <div>
-              <img src={whatlg} alt="" />
-            </div>
-            {/* 2 */}
-            <div>
-              <img src={whylg} alt="" />
-            </div>
-          </div>
-          {/*  */}
-          <div className="md:flex justify-end pt-12 hidden">
-            <img src={bar} alt="" />
-          </div>
+        <section>
+        <div>
+            <h1 className="font-extrabold text-5xl leading-[68px] lg:text-[9.3rem] lg:leading-[12rem] uppercase header">About us</h1>
         </div>
+        <div className="pt-7 pb-8 px-10 flex flex-col gap-y-8 md:flex-row md:px-24 md:gap-x-[6.5rem]  xl:justify-center">
+            <div className="px-5 pt-5 pb-4 md:px-8 about border-2 border-black xl:max-w-lg xl:py-8">
+                <h3 className="text-[#2B5EC0] text-xl font-bold md:text-3xl">What is eDucura?</h3>
+                <p className="text-justify font-normal text-xs leading-5 md:text-base md:leading-8 md:mt-5">We are a cloud based e-management platform that help schools across Africa to optimize daily operations to drive data analytics with insight into the school activities ranging from registration, attendance, results, tests, exams, school fees payment and we also function as a way to bridge the school-parent gap.</p>
+            </div>
+            <div className="px-5 pt-5 pb-4 md:px-8 about border-2 border-black xl:max-w-lg xl:py-8">
+                <h3 className="text-[#2B5EC0] text-xl font-bold md:text-3xl" >Why eDucura?</h3>
+                <p className="text-justify font-normal text-xs leading-5 md:text-base md:leading-8 md:mt-5">Via our platform parents can get real time update on their kids performance in school via our website, email and SMS. With our platform schools no longer have to struggle to deliver updates to parents and fees payment has been completely streamlined. Also parents can access the student files, results, fees reciept an lots more.</p>
+            </div>
+        </div>
+        <div className="md:flex md:justify-end md:mt-12">
+            <div className="px-[3.3rem] pt-5 pb-3 md:w-[50vw] md:pl-28 md:pr-52 bg-[#2B5EC0]">
+                <h3 className="text-white font-bold text-xl md:text-3xl">Benefits of using eDuruca?</h3>
+                <p className="text-justify text-xs mt-1 leading-6 md:text-base md:leading-8 text-white">We have a world className simple and easy-to-use spftware that is customized uniquely for your school. <strong> It takes less than 5 minutes to set up and account with us.</strong></p>
+                <div className="flex justify-end mt-6">
+                    <a href="" className="text-xs leading-6 md:text-base md:leading-8 text-white">Click here to begin <i className="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
     </React.Fragment>
   );
 }
