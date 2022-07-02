c6edbb9b689d6da0df0f6bfa4b0ee9c07a750878
Daramfon Ukpong <kandysfirst@gmail.com>
2022-07-02 04:50:22 +0100

    REMOVED TAILWIND 'FONT-SANS' PROPERTY

--- src/Navbars/HomeNav.jsx
+++ src/Navbars/HomeNav.jsx
@@ -1,71 +1,90 @@
 import React, { useState } from "react";
-import cap from './images/Vector.png';
-import { Link } from 'react-router-dom';
-import cap2 from './images/Vector2.png';
+import cap from "./images/Vector.png";
+import { Link } from "react-router-dom";
+import cap2 from "./images/Vector2.png";
+import "../Assets/Contact.css";
 
 function HomeNav() {
-    const [open, setOpen] = useState(false);
-  return(
+  const [open, setOpen] = useState(false);
+  return (
     <React.Fragment>
-    {/* mobile view */}
-        <div className="h-[100px] flex md:hidden justify-between items-center">
-            <div className="flex justify-center items-center ml-[20px]">
-                    <div className="bg-[#2B5EC0] h-[38px] w-[38px] rounded-full flex justify-center items-center">
-                        <img src={cap2} alt="" />
-                    </div>
-                    {/* head text */}
-                    <div className="pt-4 select-none">
-                        <h1 className="text-[13.8182px] text-[#2B5EC0] pl-2 font-sans">eDucura</h1>
-                        <h1 className="text-[4.14545px] capitalize font-sans">...promoting educational exelence</h1>
-                    </div>
-                </div>
-                {/* hamburger */}
-                <div onClick={() => setOpen(!open)} className="text-3xl flex justify-center items-center mr-[36px] cursor-pointer">
-                    <ion-icon name={ open ? 'close' : 'menu' }></ion-icon>
-                </div>
-                {/* links */}
+      {/* mobile view */}
+      <div className="h-[100px] flex md:hidden justify-between items-center">
+        <div className="flex justify-center items-center ml-[20px]">
+          <div className="bg-[#2B5EC0] h-[38px] w-[38px] rounded-full flex justify-center items-center">
+            <img src={cap2} alt="" />
+          </div>
+          {/* head text */}
+          <div className="pt-4 select-none">
+            <h1 className="text-[13.8182px] text-[#2B5EC0] pl-2 ">eDucura</h1>
+            <h1 className="text-[4.14545px] capitalize ">
+              ...promoting educational exelence
+            </h1>
+          </div>
         </div>
-            <div className=''>
-                <ul className={(open ? 'opacity-96' : 'opacity-0') + ' bg-[#f7f7f7] h-[800px] w-full text-center absolute transition-all duration-500 py-48'}>
-                <Link to='/'>
-                    <li className="text-[16px] font-[400] p-6 text-xl">Home</li>
-                </Link>
-                <Link to='/about-us'>
-                    <li className="text-[16px] font-[400] p-6 text-xl">About Us</li>
-                </Link>
-                <Link to='/contact-us'>
-                    <li className="text-[16px] font-[400] p-6 text-xl">Contact Us</li>
-                </Link>
-                </ul>
-            </div>
-    {/* lg screen */}
-        <div className="h-[100px] hidden md:flex justify-between items-center">
-            {/* logo */}
-            <div className="flex justify-center items-center ml-[70px]">
-                <div className="bg-[#2B5EC0] h-[55px] w-[55px] rounded-full flex justify-center items-center">
-                    <img src={cap} alt="" />
-                </div>
-                {/* head text */}
-                <div className="pt-4 select-none">
-                    <h1 className="text-[20px] text-[#2B5EC0] pl-2 font-sans">eDucura</h1>
-                    <h1 className="text-[6px] capitalize font-sans">...promoting educational exelence</h1>
-                </div>
-            </div>
-            {/* navigation */}
-            <div className="flex justify-center items-center relative">
-                <ul className='flex space-x-7 mr-[68px]'>
-                    <Link to='/'>
-                        <li className="text-[16px] font-[400]">Home</li>
-                    </Link>
-                    <Link to='/about-us'>
-                        <li className="text-[16px] font-[400]">About Us</li>
-                    </Link>
-                    <Link to='/contact-us'>
-                        <li className="text-[16px] font-[400]">Contact Us</li>
-                    </Link>
-                </ul>
-            </div>
+        {/* hamburger */}
+        <div
+          onClick={() => setOpen(!open)}
+          className="text-3xl flex justify-center items-center mr-[36px] cursor-pointer"
+        >
+          <ion-icon name={open ? "close" : "menu"}></ion-icon>
         </div>
+        {/* links */}
+      </div>
+      <div className="">
+        <ul
+          className={
+            (open ? "opacity-96" : "opacity-0") +
+            " bg-[#f7f7f7] h-[auto] w-full text-center absolute transition-all duration-500 py-48"
+          }
+        >
+          <Link to="/">
+            <li className="text-[16px] font-[400] p-6 text-xl font-sans">
+              Home
+            </li>
+          </Link>
+          <Link to="/about-us">
+            <li className="text-[16px] font-[400] p-6 text-xl font-sans">
+              About Us
+            </li>
+          </Link>
+          <Link to="/contact-us">
+            <li className="text-[16px] font-[400] p-6 text-xl font-sans">
+              Contact Us
+            </li>
+          </Link>
+        </ul>
+      </div>
+      {/* lg screen */}
+      <div className="h-[100px] hidden md:flex justify-between items-center">
+        {/* logo */}
+        <div className="flex justify-center items-center ml-[70px]">
+          <div className="bg-[#2B5EC0] h-[55px] w-[55px] rounded-full flex justify-center items-center">
+            <img src={cap} alt="" />
+          </div>
+          {/* head text */}
+          <div className="pt-4 select-none">
+            <h1 className="text-[20px] text-[#2B5EC0] pl-2">eDucura</h1>
+            <h1 className="text-[6px] capitalize">
+              ...promoting educational exelence
+            </h1>
+          </div>
+        </div>
+        {/* navigation */}
+        <div className="flex justify-center items-center relative">
+          <ul className="flex space-x-7 mr-[68px]">
+            <Link to="/">
+              <li className="text-[16px] font-[400]">Home</li>
+            </Link>
+            <Link to="/about-us">
+              <li className="text-[16px] font-[400]">About Us</li>
+            </Link>
+            <Link to="/contact-us">
+              <li className="text-[16px] font-[400]">Contact Us</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
