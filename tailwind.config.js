00bb92df358c3878b0a991cdc701c8d153de41cc
Daramfon Ukpong <kandysfirst@gmail.com>
2022-07-02 04:47:03 +0100

    ADDED CUSTOM VIEWPORT

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,19 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+
+  // ADDED CUSTOM IEWPORT FOR SMALL DEVICES
   theme: {
-    extend: {},
+    maxWidth: {
+      '4/5': '80%'
+    },
+
+    extend: {
+      screens: {
+        'sm': '320px',
+        // => @media (min-width: 320px) { ... }
+      },
+    },
   },
   plugins: [],
 }
